3ef824a4870d73a7d5fdeeb7f7a9026db20ca79d
ceko98 <ceko9898@gmail.com>
2022-11-09 23:00:24 +0200

    API initial layout

---
+++ api/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { Server } from 'typescript-rest';
+import { StockMarketService } from './service/stock-market';
+
+const app = express();
+const port = 3000;
+
+Server.buildServices(app, StockMarketService);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ api/src/service/stock-market.ts
@@ -0,0 +1,11 @@
+import { GET, Path } from 'typescript-rest'
+
+@Path('/stock-market')
+export class StockMarketService {
+  
+  @GET
+  @Path('/')
+  pointsOfInterest() {
+    
+  }
+}
